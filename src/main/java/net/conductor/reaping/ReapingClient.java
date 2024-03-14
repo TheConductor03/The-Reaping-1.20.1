54b2cd5c36167927a37e50c35e87460c8026292d
alhar <theconductor0310@gmail.com>
2024-03-14 19:00:21 -0400

    Project Commit

---
+++ src/main/java/net/conductor/reaping/ReapingClient.java
@@ -0,0 +1,10 @@
+package net.conductor.reaping;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReapingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
